e596152ce9ea8359c7719e429457992c9626f811
abhi <abhiifour@gmail.com>
2026-06-08 22:26:23 +0530

    many fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import DemoLoader from "@/components/ui/demo-loader";
 import { ICON_LIST } from "@/icons";
-import Activity from "@/icons/activity";
-import Airplay from "@/icons/airplay";
-import AlarmClock from "@/icons/alarm-clock";
-import Shell from "@/icons/shell";
 
 export default function Page() {
   return (
@@ -29,6 +25,7 @@ export default function Page() {
         <div>
           <p className="text-gray-400 mt-2 text-sm">Grained on top of <span className="border px-1 border-[#5FB0E8]">Lucide</span></p>
           <p className="text-gray-400 text-end text-sm">Open Source</p>
+          <p className="text-gray-400 text-end text-sm">v 0.1.0</p>
         </div>
       </div>
       <p className="text-xs mt-12 text-end lowercase">{ICON_LIST.length} total </p>
@@ -37,7 +34,7 @@ export default function Page() {
       
        {
         ICON_LIST.map((icon,index) => (
-          <div key={index} className="ring ring-neutral-100 hover:ring-[#5FB0E8] flex items-center justify-center flex-col py-4">
+          <div key={index} className="ring ring-gray-100 hover:ring-[#5FB0E8] flex items-center justify-center flex-col py-4">
             <icon.icon/>
             <p className="lowercase text-gray-400 text-sm">{icon.title}</p>
           </div>
