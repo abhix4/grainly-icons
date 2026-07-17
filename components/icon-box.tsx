d2f7675547e7d7089fa7f1ba75d82f30fe399144
abhix4 <abhiifour@gmail.com>
2026-07-17 20:15:47 +0530

    fix: copy command

--- components/icon-box.tsx
+++ components/icon-box.tsx
@@ -19,7 +19,7 @@ export default function IconBox({ icon: Icon, title }: IconBoxProps){
 
                  <Tooltip>
                     <TooltipTrigger asChild>
-                        <div className="absolute  top-3 right-3 rounded-sm bg-gray-100 hover:bg-gray-200 p-1 cursor-pointer opacity-0 group-hover:opacity-100" onClick={() => copy(`pnpm dlx shadcn@latest add "https://lucide-animated.com/r/air-vent.json"`)}>
+                        <div className="absolute  top-3 right-3 rounded-sm bg-gray-100 hover:bg-gray-200 p-1 cursor-pointer opacity-0 group-hover:opacity-100" onClick={() => copy(`pnpm dlx shadcn@latest add "https://grainlyicons.abhii.space/r/${title}.json"`)}>
                           {
                             copiedText ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-check-icon lucide-check"><path d="M20 6 9 17l-5-5"/></svg> :   <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className=" lucide lucide-terminal-icon lucide-terminal"><path d="M12 19h8"/><path d="m4 17 6-6-6-6"/></svg>
                           }
